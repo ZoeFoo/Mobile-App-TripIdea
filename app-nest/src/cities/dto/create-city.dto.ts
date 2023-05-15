1a1064f17e9652f5a17bd69a88f18aa18962470c
Zoe Foo <foochormei335205@gmail.com>
2023-05-16 00:09:44 +0800

    finished cities entities and tdo

--- app-nest/src/cities/dto/create-city.dto.ts
+++ app-nest/src/cities/dto/create-city.dto.ts
@@ -1,1 +1,4 @@
-export class CreateCityDto {}
+import { OmitType } from "@nestjs/swagger";
+import { City } from "../entities/city.entity";
+
+export class CreateCityDto extends OmitType(City, ['id'] as const) { }
--- app-nest/src/cities/entities/city.entity.ts
+++ app-nest/src/cities/entities/city.entity.ts
@@ -1,1 +1,24 @@
-export class City {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
+
+export class City {
+    @ApiProperty({ default: 1 })
+    @IsInt()
+    @IsNotEmpty()
+    id: number;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    engName: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    chName: string;
+
+    @ApiProperty()
+    @IsInt()
+    @IsNotEmpty()
+    areaId: number;
+}
