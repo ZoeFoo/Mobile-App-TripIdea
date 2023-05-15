89e6abd67776f6fe7ff224def37fd7637db0901d
Zoe Foo <foochormei335205@gmail.com>
2023-05-16 00:02:30 +0800

    finished updated days entities and dto

--- app-nest/src/days/dto/create-day.dto.ts
+++ app-nest/src/days/dto/create-day.dto.ts
@@ -1,1 +1,4 @@
-export class CreateDayDto {}
+import { OmitType } from "@nestjs/swagger";
+import { Day } from '../entities/day.entity';
+
+export class CreateDayDto extends OmitType(Day, ['id'] as const) { }
--- app-nest/src/days/entities/day.entity.ts
+++ app-nest/src/days/entities/day.entity.ts
@@ -1,1 +1,19 @@
-export class Day {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsNotEmpty } from "class-validator";
+
+export class Day {
+    @ApiProperty({ default: 1 })
+    @IsInt()
+    @IsNotEmpty()
+    id: number;
+
+    @ApiProperty()
+    @IsInt()
+    @IsNotEmpty()
+    travelPlanId: number;
+
+    @ApiProperty()
+    @IsInt()
+    @IsNotEmpty()
+    whichDay: number;
+}
