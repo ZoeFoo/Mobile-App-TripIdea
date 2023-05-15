0ba72ef083d9a4fed611d366bbac379c40b765aa
Zoe Foo <foochormei335205@gmail.com>
2023-05-15 23:59:30 +0800

    finished updated locations entities and tdo

--- app-nest/src/locations/entities/location.entity.ts
+++ app-nest/src/locations/entities/location.entity.ts
@@ -1,1 +1,67 @@
-export class Location {}
+import { ApiProperty } from "@nestjs/swagger";
+import {  IsInt, IsNotEmpty, IsString } from "class-validator";
+
+export class Location {
+    @ApiProperty({ default: 1 })
+    @IsInt()
+    @IsNotEmpty()
+    id: number;
+
+    @ApiProperty()
+    @IsInt()
+    @IsNotEmpty()
+    travelPlanDayId: number;
+
+    @ApiProperty()
+    @IsInt()
+    @IsNotEmpty()
+    countryId: number;
+
+    @ApiProperty({ default: null })
+    @IsInt()
+    areaId: number;
+
+    @ApiProperty({ default: null })
+    @IsInt()
+    cityId: number;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    locationName: string;
+
+    @ApiProperty()
+    @IsString()
+    locationImg: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    category: string;
+
+    @ApiProperty()
+    @IsString()
+    address: string;
+
+    @ApiProperty()
+    @IsString()
+    tel: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    startTime: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    endTime: string;
+
+    @ApiProperty()
+    @IsString()
+    transportation: string;
+
+    @ApiProperty()
+    @IsString()
+    notes: string;
+}
