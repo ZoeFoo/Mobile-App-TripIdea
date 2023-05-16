ecc9e3ca41e6fd450c95c1c85fd61f5cb8e3a76b
Zoe Foo <foochormei335205@gmail.com>
2023-05-16 18:05:20 +0800

    updated travel-plans CRUD

--- app-nest/src/travel-plans/travel-plans.controller.ts
+++ app-nest/src/travel-plans/travel-plans.controller.ts
@@ -1,34 +1,133 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import * as fs from 'fs';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseInterceptors, UploadedFile, NotFoundException, Query, UseGuards, Request, Headers } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { jwtConstants } from 'src/auth/constants';
+import { JwtService } from '@nestjs/jwt';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { TravelPlansService } from './travel-plans.service';
 import { CreateTravelPlanDto } from './dto/create-travel-plan.dto';
 import { UpdateTravelPlanDto } from './dto/update-travel-plan.dto';
+import { CreateDayDto } from '../days/dto/create-day.dto';
 
+const allowedMimeTypes = [
+    "image/jpeg",
+    "image/png",
+];
+
+@UseGuards(AuthGuard('jwt'))
+@ApiTags('travel-plans')
 @Controller('travel-plans')
 export class TravelPlansController {
-  constructor(private readonly travelPlansService: TravelPlansService) {}
+    constructor(
+        private readonly travelPlansService: TravelPlansService,
+        private jwtService: JwtService) { }
 
-  @Post()
-  create(@Body() createTravelPlanDto: CreateTravelPlanDto) {
-    return this.travelPlansService.create(createTravelPlanDto);
-  }
+    @Post()
+    @UseInterceptors(FileInterceptor('thumbnailFile'))
+    async create(
+        @Body() createTravelPlanDto: CreateTravelPlanDto,
+        @UploadedFile() thumbnailFile,
+        @Body() createTravelPlanDayDto: CreateDayDto,
+        @Headers("authorization") authHeader
+    ) {
+        //console.log({ thumbnailFile });
 
-  @Get()
-  findAll() {
-    return this.travelPlansService.findAll();
-  }
+        let access_token = authHeader.replace("Bearer ", "");
+        //console.log("POST /travel-plans access_token: ", access_token);
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.travelPlansService.findOne(+id);
-  }
+        let userObject = await this.jwtService.verifyAsync(access_token, {
+            secret: jwtConstants.secret
+        });
+        //console.log("POST /travel-plans userObject: ", userObject);
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTravelPlanDto: UpdateTravelPlanDto) {
-    return this.travelPlansService.update(+id, updateTravelPlanDto);
-  }
+        const data: any = {};
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.travelPlansService.remove(+id);
-  }
+        for (let key in createTravelPlanDto) {
+            if (key == "thumbnailFile") continue;
+            data[key] = createTravelPlanDto[key];
+        }
+
+        if (thumbnailFile) {
+            data.thumbnail = thumbnailFile.path;
+        }
+
+        if (thumbnailFile && (!thumbnailFile?.mimetype || !allowedMimeTypes.includes(thumbnailFile.mimetype.toLowerCase()))) {
+            fs.unlinkSync(thumbnailFile.path)
+            throw new NotFoundException("Invalid image type")
+        }
+
+        data.authorId = userObject.id;
+        data.startDay = new Date(`${createTravelPlanDto.startDay}`);
+        data.endDay = new Date(`${createTravelPlanDto.endDay}`);
+
+        return await this.travelPlansService.create(data, createTravelPlanDayDto);
+    }
+
+    @Get()
+    async findAll(): Promise<any> {
+        // console.log(this.travelPlansService.findAll())
+        return await this.travelPlansService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return await this.travelPlansService.findOne(+id);
+    }
+
+    @Get('/myselfPlan/:id')
+    async findMyselfPlan(@Param('id') id: string, @Query('name') name: string): Promise<any> {
+        // console.log(this.travelPlansService.findMyselfPlan(+id, name))
+        return await this.travelPlansService.findMyselfPlan(+id, name);
+    }
+
+    @Get('/hitPlan')
+    async findHitPlan(@Query('name') name: string): Promise<any> {
+        // console.log(this.travelPlansService.findHitPlan(name))
+        return await this.travelPlansService.findHitPlan(name);
+    }
+
+    @Get('/selfCreatedPlan')
+    async findPlansCreatedByUser(@Request() req, @Headers("authorization") authHeader) {
+        let access_token = authHeader.replace("Bearer ", "");
+
+        // console.log("access_token: ", access_token);
+        let userObject = await this.jwtService.verifyAsync(access_token, {
+            secret: jwtConstants.secret
+        });
+        return await this.travelPlansService.findPlansCreatedByUser(userObject.id);
+    }
+
+    @Patch(':id')
+    @UseInterceptors(FileInterceptor('thumbnailFile'))
+    async update(
+        @Param('id', ParseIntPipe) id: string,
+        @Body() updateTravelPlanDto: UpdateTravelPlanDto,
+        @UploadedFile() thumbnailFile,) {
+        const data: any = {};
+        for (let key in updateTravelPlanDto) {
+            if (key == "thumbnailFile") continue;
+            data[key] = updateTravelPlanDto[key];
+        }
+
+        if (thumbnailFile) {
+            data.thumbnail = thumbnailFile.path
+        }
+
+        if (thumbnailFile && (!thumbnailFile?.mimetype || !allowedMimeTypes.includes(thumbnailFile.mimetype.toLowerCase()))) {
+            fs.unlinkSync(thumbnailFile.path)
+            throw new NotFoundException("Invalid image type")
+        }
+
+        data.authorId = parseInt(`${updateTravelPlanDto.authorId}`);
+        data.startDay = new Date(`${updateTravelPlanDto.startDay}`);
+        data.endDay = new Date(`${updateTravelPlanDto.endDay}`);
+
+        return await this.travelPlansService.update(+id, data);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id', ParseIntPipe) id: string) {
+        return await this.travelPlansService.remove(+id);
+    }
 }
--- app-nest/src/travel-plans/travel-plans.module.ts
+++ app-nest/src/travel-plans/travel-plans.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma/dist/prisma.service';
 import { TravelPlansService } from './travel-plans.service';
 import { TravelPlansController } from './travel-plans.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { UsersService } from 'src/users/users.service';
+import { SharesService } from 'src/shares/shares.service';
+import { DaysService } from 'src/days/days.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
-  controllers: [TravelPlansController],
-  providers: [TravelPlansService]
+    imports: [
+        MulterModule.register({
+            limits: { fieldSize: 25 * 1024 * 1024 },
+            storage: diskStorage({
+                destination: './public/travelPlan_thumbnalis',
+                filename: (req, file, cb) => {
+                    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+                    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1])
+                },
+            }),
+        }),
+        JwtModule.register({
+            secret: jwtConstants.secret, 
+            signOptions: { expiresIn: '7d' },
+        })
+    ],
+    controllers: [TravelPlansController],
+    providers: [
+        TravelPlansService,
+        PrismaService,
+        UsersService,
+        SharesService,
+        DaysService,
+        JwtStrategy]
 })
-export class TravelPlansModule {}
+export class TravelPlansModule { }
