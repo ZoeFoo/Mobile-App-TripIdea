cbd414f64b4b6604cd778dbf425ab64e7a0349e4
Zoe Foo <foochormei335205@gmail.com>
2023-05-16 23:47:16 +0800

    updated Area and Country CRUD

--- app-nest/src/areas/areas.service.ts
+++ app-nest/src/areas/areas.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma/dist/prisma.service';
 import { CreateAreaDto } from './dto/create-area.dto';
 import { UpdateAreaDto } from './dto/update-area.dto';
+import { Area } from './entities/area.entity';
 
 @Injectable()
 export class AreasService {
-  create(createAreaDto: CreateAreaDto) {
-    return 'This action adds a new area';
-  }
+    constructor(private prisma: PrismaService) { }
 
-  findAll() {
-    return `This action returns all areas`;
-  }
+    async create(createAreaDto: CreateAreaDto) {
+        let area = await this.prisma.area.create({ data: { ...createAreaDto } })
+        //console.log(areas)
+        return area;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} area`;
-  }
+    async findAll(): Promise<Area[]> {
+        return await this.prisma.area.findMany();
+    }
 
-  update(id: number, updateAreaDto: UpdateAreaDto) {
-    return `This action updates a #${id} area`;
-  }
+    async findAreas(id: number) {
+        let foundAreas = await this.prisma.area.findMany(
+            { where: { countryId: id } }
+        )
+        // console.log('foundAreas', foundAreas)
 
-  remove(id: number) {
-    return `This action removes a #${id} area`;
-  }
+        if (!foundAreas) throw new NotFoundException('Area not found!')
+        return foundAreas;
+    }
+
+    async findOne(id: number) {
+        let foundArea = await this.prisma.area.findFirst(
+            { where: { id } }
+        )
+
+        if (!foundArea) throw new NotFoundException('Area not found!')
+        return foundArea;
+    }
+
+    async update(id: number, updateAreaDto: UpdateAreaDto) {
+        let result = await this.prisma.area.update({
+            where: { id },
+            data: updateAreaDto
+        })
+        return result;
+    }
+
+    async remove(id: number) {
+        let result = await this.prisma.area.delete({ where: { id } })
+        return result;
+    }
 }
--- app-nest/src/countries/countries.module.ts
+++ app-nest/src/countries/countries.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma/dist/prisma.service';
 import { CountriesService } from './countries.service';
 import { CountriesController } from './countries.controller';
 
 @Module({
   controllers: [CountriesController],
-  providers: [CountriesService]
+    providers: [CountriesService, PrismaService]
 })
 export class CountriesModule {}
