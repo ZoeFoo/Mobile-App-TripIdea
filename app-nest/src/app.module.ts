6cbef114c365ddcbb8784ab3e7cf26e5b6f161e7
Zoe Foo <foochormei335205@gmail.com>
2023-05-16 13:22:46 +0800

    import PrismaModule in app module

--- app-nest/src/app.module.ts
+++ app-nest/src/app.module.ts
@@ -1,4 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { PrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -12,14 +13,27 @@ import { FollowersModule } from './followers/followers.module';
 import { TravelPlansModule } from './travel-plans/travel-plans.module';
 import { SharesModule } from './shares/shares.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
-import { TransporationsModule } from './transporations/transporations.module';
 import { LocationCategoryModule } from './location-category/location-category.module';
 import { TransportationsModule } from './transportations/transportations.module';
 
 @Module({
-  imports: [UsersModule, TravelPlansModule, LocationsModule, DaysModule, LikesModule, CountriesModule, AreasModule, CitiesModule, FollowersModule, SharesModule, TransporationsModule, LocationCategoryModule, TransportationsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        PrismaModule.forRoot(),
+        UsersModule,
+        TravelPlansModule,
+        LocationsModule,
+        DaysModule,
+        LikesModule,
+        CountriesModule,
+        AreasModule,
+        CitiesModule,
+        FollowersModule,
+        SharesModule,
+        LocationCategoryModule,
+        TransportationsModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 
 export class AppModule implements NestModule {
--- app-nest/src/travel-plans/entities/travel-plan.entity.ts
+++ app-nest/src/travel-plans/entities/travel-plan.entity.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsInt, IsNotEmpty, IsString } from "class-validator";
+import { Status } from '@prisma/client';
 
 export class TravelPlan {
     @ApiProperty({ default: 1 })
