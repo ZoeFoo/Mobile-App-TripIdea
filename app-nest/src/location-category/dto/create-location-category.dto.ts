13d6282f319e8d01dcf1ab250ca0d19208a1bb7f
Zoe Foo <foochormei335205@gmail.com>
2023-05-16 12:41:51 +0800

    updated LocationCategory entities and tdo and schema

--- app-nest/src/location-category/dto/create-location-category.dto.ts
+++ app-nest/src/location-category/dto/create-location-category.dto.ts
@@ -1,1 +1,4 @@
-export class CreateLocationCategoryDto {}
+import { OmitType } from "@nestjs/swagger";
+import { LocationCategory } from '../entities/location-category.entity';
+
+export class CreateLocationCategoryDto extends OmitType(LocationCategory, ['id'] as const) { }
--- app-nest/src/location-category/entities/location-category.entity.ts
+++ app-nest/src/location-category/entities/location-category.entity.ts
@@ -1,1 +1,14 @@
-export class LocationCategory {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
+
+export class LocationCategory {
+    @ApiProperty({ default: 1 })
+    @IsInt()
+    @IsNotEmpty()
+    id: number;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+}
